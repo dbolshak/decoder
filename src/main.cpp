ee745c41301c85abfe276c2a76e60ac2e7df7edc
Denis, Bolshakov <bolshakov.denis@gmail.com>
2015-01-03 15:20:34 -0500

    adding code to read gzip

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 #include <google/protobuf/io/gzip_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
 using namespace google::protobuf::io;
+using namespace std;
 
 int main(int argc, const char** argv)
 {
@@ -13,6 +17,16 @@ int main(int argc, const char** argv)
     FileInputStream* fileInputStream = new FileInputStream(fileno(fd), -1);
     GzipInputStream* gzipInputStream = new GzipInputStream(fileInputStream);
 
+    string outputName = argv[1];
+    outputName += ".out";
+    ofstream out(outputName.c_str(), ios::out | ios::app | ios::binary);
+    int size = 2048;
+    const void* buffer = malloc(size);
+    while (gzipInputStream->Next(&buffer, &size)) {
+      out.write((const char*)buffer, size);
+    }
+
+    cout << gzipInputStream->ZlibErrorMessage();
     delete gzipInputStream;
     delete fileInputStream;
     fclose(fd);
